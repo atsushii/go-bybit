2a1e6e7573f0e8a949f1e975ff36c0f2e5a45257
Atsushi <atsushimiyamoto07@gmail.com>
2021-11-28 16:27:23 +0900

    return base endpoint according to flag

--- v1/client.go
+++ v1/client.go
@@ -8,20 +8,31 @@ const (
 	baseAPITestnetURL = "https://api-testnet.bybit.com"
 )
 
+// useTestnet switch all the API endpoints from production to the testnet
+var useTestnet = false
+
 func currentTimestamp() int64 {
 	return formatTimestamp(time.Now())
 }
 
+// time into Unix timestamp in milliseconds
 func formatTimestamp(t time.Time) int64 {
 	return t.UnixNano() / int64(time.Millisecond)
 }
-
 
 // Client define API client
 type client struct {
 	APIKey    string
 	SecretKey string
 	Debug     bool
+}
+
+// return endpoint of the REST API according to the useTestnet flag
+func getAPIEndpoint() string {
+	if useTestnet {
+		return baseAPITestnetURL
+	}
+	return baseAPIMainURL
 }
 
 // newClient initialize an API client instance with API key and secret key.
