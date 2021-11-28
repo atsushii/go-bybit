2eac32996454a806c35e84e12b7be2ed38b37cc9
Atsushi <atsushimiyamoto07@gmail.com>
2021-11-28 20:13:35 +0900

    define type for request

---
+++ v1/request.go
@@ -0,0 +1,20 @@
+package bybit
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type params map[string]interface{}
+
+type request struct {
+	method string
+	endpoint string
+	query url.Values
+	form url.Values
+	header http.Header
+	body io.Reader
+	fullURL string
+	sign string
+}
